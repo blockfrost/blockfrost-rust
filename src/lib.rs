3c83a4bcb95ffddca154334058c8861d2e461935
João M. Bezerra <marcospb19@hotmail.com>
2021-08-16 17:35:11 -0300

    Adding BlockFrostApi and Settings structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
-//! BlockFrost sdk for Rust.
+//! Rust SDK for Blockfrost.io
+
+pub const CARDANO_MAINNET_NETWORK: &str = "https://cardano-mainnet.blockfrost.io/api/v0";
+pub const CARDANO_TESTNET_NETWORK: &str = "https://cardano-testnet.blockfrost.io/api/v0";
+
+#[derive(Debug, Default)]
+pub struct BlockFrostApi {
+    public_id: String,
+    settings: Settings,
+}
+
+impl BlockFrostApi {
+    pub fn new(public_id: impl AsRef<str>, settings: Settings) -> Self {
+        Self {
+            public_id: public_id.as_ref().to_string(),
+            settings,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Settings;
