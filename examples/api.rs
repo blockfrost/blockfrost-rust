4378a82f77f88e1c33da5d53e71d7e908b6f823c
João M. Bezerra <marcospb19@hotmail.com>
2021-08-17 20:03:51 -0300

    Modify example to accept key from .env

--- examples/api.rs
+++ examples/api.rs
@@ -4,9 +4,9 @@ type MainResult = std::result::Result<(), Box<dyn std::error::Error + Send + Syn
 
 #[tokio::main]
 async fn main() -> MainResult {
-    let project_id = "YOUR_KEY_HERE";
+    let project_id = std::fs::read_to_string(".env").unwrap();
     let settings = Settings::new().set_test_network(true);
-    let api = BlockFrostApi::new(project_id, settings);
+    let api = BlockFrostApi::new(project_id.trim(), settings);
 
     let health = api.health().await;
     println!("{:?}", health);
