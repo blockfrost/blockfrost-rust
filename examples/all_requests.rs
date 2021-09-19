1c4c356c1a139f792976b66bb9bb3fcf5a9f0948
João M. Bezerra <marcospb19@hotmail.com>
2021-09-19 05:19:14 +0000

    Make examples use mainnet instead of testnet

--- examples/all_requests.rs
+++ examples/all_requests.rs
@@ -2,7 +2,7 @@ use blockfrost::{env, BlockFrostApi, Settings};
 
 fn build_api() -> blockfrost::Result<BlockFrostApi> {
     let project_id = env::load_project_id()?.expect("BLOCKFROST_PROJECT_ID not found.");
-    let settings = Settings::new(project_id).set_test_network(true);
+    let settings = Settings::new(project_id);
     let api = BlockFrostApi::new(settings);
 
     Ok(api)
