fd0d1a37a113a6b9005b0d906569d39258add270
João M. Bezerra <marcospb19@hotmail.com>
2021-10-12 18:59:45 -0300

    Add example of how to submit a transaction

---
+++ examples/submit_transaction.rs
@@ -0,0 +1,21 @@
+use blockfrost::{env, BlockFrostApi, Settings};
+
+fn build_api() -> blockfrost::Result<BlockFrostApi> {
+    let project_id = env::load_project_id()?.expect("BLOCKFROST_PROJECT_ID not found.");
+    let api = BlockFrostApi::new(project_id, Settings::new());
+    Ok(api)
+}
+
+#[tokio::main]
+async fn main() -> blockfrost::Result<()> {
+    let api = build_api()?;
+
+    // Should contain the correct cbor contents
+    let transaction_data = vec![0; 1024]; // Just an example (will fail)
+    let transaction_hash = api.transactions_submit(transaction_data).await?;
+
+    // At this point, you should probably save the transaction_hash
+    println!("{}", transaction_hash);
+
+    Ok(())
+}
